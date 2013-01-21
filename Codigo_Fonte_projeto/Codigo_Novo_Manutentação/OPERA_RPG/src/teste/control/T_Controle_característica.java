478649fb458672cff9257026f0c1d5a1dda50b75
Tallys Martins <tallysmartins@yahoo.com.br>
2013-01-21 10:52:54 -0200

    Criação do pacote de teste das classes controle

---
+++ Codigo_Fonte_projeto/Codigo_Novo_Manutentação/OPERA_RPG/src/teste/control/T_Controle_característica.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste.control;
+
+/**
+ *
+ * @author Administrador
+ */
+public class T_Controle_característica {
+    
+}
