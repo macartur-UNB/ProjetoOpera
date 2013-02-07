7e70d96ec38cb0d21fdb046bacfee1fcaf077bb5
macartur <macartur.sc@gmail.com>
2013-02-07 19:35:29 -0200

    teste

---
+++ Codigo_Fonte_projeto/Codigo_Novo_Manutentação/OPERA_RPG/test/model/dao/DAO_JogoTest.java
@@ -0,0 +1,366 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.dao;
+
+import model.classes.Jogo;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Macartur
+ */
+public class DAO_JogoTest {
+    
+    public DAO_JogoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of criarDiretorioJogo method, of class DAO_Jogo.
+     */
+    @Test
+    public void testCriarDiretorioJogo() throws Exception {
+        System.out.println("criarDiretorioJogo");
+        Jogo jogo = null;
+        DAO_Jogo.criarDiretorioJogo(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removerArquivosJogo method, of class DAO_Jogo.
+     */
+    @Test
+    public void testRemoverArquivosJogo() throws Exception {
+        System.out.println("removerArquivosJogo");
+        Jogo jogo = null;
+        DAO_Jogo.removerArquivosJogo(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removerDiretorioJogo method, of class DAO_Jogo.
+     */
+    @Test
+    public void testRemoverDiretorioJogo() throws Exception {
+        System.out.println("removerDiretorioJogo");
+        Jogo jogo = null;
+        DAO_Jogo.removerDiretorioJogo(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of criarDiretorioFichas method, of class DAO_Jogo.
+     */
+    @Test
+    public void testCriarDiretorioFichas() throws Exception {
+        System.out.println("criarDiretorioFichas");
+        Jogo jogo = null;
+        DAO_Jogo.criarDiretorioFichas(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removerArquivosFichas method, of class DAO_Jogo.
+     */
+    @Test
+    public void testRemoverArquivosFichas() throws Exception {
+        System.out.println("removerArquivosFichas");
+        Jogo jogo = null;
+        DAO_Jogo.removerArquivosFichas(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removerDiretorioFichas method, of class DAO_Jogo.
+     */
+    @Test
+    public void testRemoverDiretorioFichas() throws Exception {
+        System.out.println("removerDiretorioFichas");
+        Jogo jogo = null;
+        DAO_Jogo.removerDiretorioFichas(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of criarDiretorioHabilidades method, of class DAO_Jogo.
+     */
+    @Test
+    public void testCriarDiretorioHabilidades() throws Exception {
+        System.out.println("criarDiretorioHabilidades");
+        Jogo jogo = null;
+        DAO_Jogo.criarDiretorioHabilidades(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removerArquivosHabilidades method, of class DAO_Jogo.
+     */
+    @Test
+    public void testRemoverArquivosHabilidades() throws Exception {
+        System.out.println("removerArquivosHabilidades");
+        Jogo jogo = null;
+        DAO_Jogo.removerArquivosHabilidades(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removerDiretorioHabilidades method, of class DAO_Jogo.
+     */
+    @Test
+    public void testRemoverDiretorioHabilidades() throws Exception {
+        System.out.println("removerDiretorioHabilidades");
+        Jogo jogo = null;
+        DAO_Jogo.removerDiretorioHabilidades(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of criarDiretorioCaracteristicas method, of class DAO_Jogo.
+     */
+    @Test
+    public void testCriarDiretorioCaracteristicas() throws Exception {
+        System.out.println("criarDiretorioCaracteristicas");
+        Jogo jogo = null;
+        DAO_Jogo.criarDiretorioCaracteristicas(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removerArquivosCaracteristicas method, of class DAO_Jogo.
+     */
+    @Test
+    public void testRemoverArquivosCaracteristicas() throws Exception {
+        System.out.println("removerArquivosCaracteristicas");
+        Jogo jogo = null;
+        DAO_Jogo.removerArquivosCaracteristicas(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removerDiretorioCaracteristicas method, of class DAO_Jogo.
+     */
+    @Test
+    public void testRemoverDiretorioCaracteristicas() throws Exception {
+        System.out.println("removerDiretorioCaracteristicas");
+        Jogo jogo = null;
+        DAO_Jogo.removerDiretorioCaracteristicas(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of criarDiretorioItens method, of class DAO_Jogo.
+     */
+    @Test
+    public void testCriarDiretorioItens() throws Exception {
+        System.out.println("criarDiretorioItens");
+        Jogo jogo = null;
+        DAO_Jogo.criarDiretorioItens(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removerArquivosItens method, of class DAO_Jogo.
+     */
+    @Test
+    public void testRemoverArquivosItens() throws Exception {
+        System.out.println("removerArquivosItens");
+        Jogo jogo = null;
+        DAO_Jogo.removerArquivosItens(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removerDiretoriosItens method, of class DAO_Jogo.
+     */
+    @Test
+    public void testRemoverDiretoriosItens() throws Exception {
+        System.out.println("removerDiretoriosItens");
+        Jogo jogo = null;
+        DAO_Jogo.removerDiretoriosItens(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of criarDiretorioDados method, of class DAO_Jogo.
+     */
+    @Test
+    public void testCriarDiretorioDados() throws Exception {
+        System.out.println("criarDiretorioDados");
+        Jogo jogo = null;
+        DAO_Jogo.criarDiretorioDados(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removerArquivosDados method, of class DAO_Jogo.
+     */
+    @Test
+    public void testRemoverArquivosDados() throws Exception {
+        System.out.println("removerArquivosDados");
+        Jogo jogo = null;
+        DAO_Jogo.removerArquivosDados(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removerDiretorioDados method, of class DAO_Jogo.
+     */
+    @Test
+    public void testRemoverDiretorioDados() throws Exception {
+        System.out.println("removerDiretorioDados");
+        Jogo jogo = null;
+        DAO_Jogo.removerDiretorioDados(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of criarDiretoriosJogo method, of class DAO_Jogo.
+     */
+    @Test
+    public void testCriarDiretoriosJogo() throws Exception {
+        System.out.println("criarDiretoriosJogo");
+        Jogo jogo = null;
+        DAO_Jogo.criarDiretoriosJogo(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of deletarJogo method, of class DAO_Jogo.
+     */
+    @Test
+    public void testDeletarJogo() throws Exception {
+        System.out.println("deletarJogo");
+        String nome = "";
+        DAO_Jogo.deletarJogo(nome);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of verificarDiretorioRaiz method, of class DAO_Jogo.
+     */
+    @Test
+    public void testVerificarDiretorioRaiz() throws Exception {
+        System.out.println("verificarDiretorioRaiz");
+        DAO_Jogo.verificarDiretorioRaiz();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of carregarJogo method, of class DAO_Jogo.
+     */
+    @Test
+    public void testCarregarJogo() throws Exception {
+        System.out.println("carregarJogo");
+        String nome = "";
+        DAO_Jogo.carregarJogo(nome);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getJogoRodando method, of class DAO_Jogo.
+     */
+    @Test
+    public void testGetJogoRodando() {
+        System.out.println("getJogoRodando");
+        Jogo expResult = null;
+        Jogo result = DAO_Jogo.getJogoRodando();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of jogosExistentes method, of class DAO_Jogo.
+     */
+    @Test
+    public void testJogosExistentes() {
+        System.out.println("jogosExistentes");
+        String[] expResult = null;
+        String[] result = DAO_Jogo.jogosExistentes();
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of jogoExiste method, of class DAO_Jogo.
+     */
+    @Test
+    public void testJogoExiste() {
+        System.out.println("jogoExiste");
+        String nome = "";
+        boolean expResult = false;
+        boolean result = DAO_Jogo.jogoExiste(nome);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of gravarJogo method, of class DAO_Jogo.
+     */
+    @Test
+    public void testGravarJogo() throws Exception {
+        System.out.println("gravarJogo");
+        Jogo jogo = null;
+        DAO_Jogo.gravarJogo(jogo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of criarJogo method, of class DAO_Jogo.
+     */
+    @Test
+    public void testCriarJogo() throws Exception {
+        System.out.println("criarJogo");
+        String nome = "";
+        DAO_Jogo.criarJogo(nome);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
